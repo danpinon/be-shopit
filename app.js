51355958eae462ff2098f3e08e1b2b29679a4931
danpinon <danielpformoso@gmail.com>
2021-06-11 10:29:54 -0500

    cors config locahosts

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 app.use(fileUpload())
-app.use(cors({origin: false}))
+app.use(cors({origin: ["http://localhost:3000", "https://thirsty-fermat-077770.netlify.app"]}))
 
 
 
